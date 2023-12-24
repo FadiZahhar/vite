7c6577caf618b260f155ecd42234751b9aa2e599
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-12-24 14:22:32 +0200

    finished the useEffect example

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
 import './index.css'
 //import { UseStateBasic } from './UseStateBasic.jsx'
-import { UseStateExample } from './UseStateExample.jsx'
+//import { UseStateExample } from './UseStateExample.jsx'
+import { UseEffectExample } from './UseEffectExample.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/*<App />
-    <UseStateBasic />*/}
-    <UseStateExample />
+    <UseStateBasic />
+    <UseStateExample />*/}
+    <UseEffectExample />
   </React.StrictMode>,
 )
